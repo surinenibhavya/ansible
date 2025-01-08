9c29ed097806837074eb25b18949e69ec2fdfa72
bhavyasurineni <bhavyasurinnei@gmail.com>
2025-01-08 21:59:40 +0530

    updating ./ansible

--- roles/mysql/templates/passreset.sql
+++ roles/mysql/templates/passreset.sql
@@ -1,2 +1,3 @@
-SET PASSWORD FOR 'root'@'localhost' = PASSWORD('{{RoboShop@1}}');
+new_root_password: "RoboShop@1"
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD ('{{new_root_password}}');
 uninstall plugin validate_password;
